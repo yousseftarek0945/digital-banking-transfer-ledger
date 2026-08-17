50213cc1b4eaa7578f00bd29b86f68f56cd0643e
Youssef Tarek <yousseftarek0945@gmail.com>
2026-08-17 17:04:00 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/qnbeyondplus/digital_banking_transfer_ledger/DigitalBankingTransferLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.qnbeyondplus.digital_banking_transfer_ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingTransferLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankingTransferLedgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qnbeyondplus/digital_banking_transfer_ledger/DigitalBankingTransferLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qnbeyondplus.digital_banking_transfer_ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingTransferLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
